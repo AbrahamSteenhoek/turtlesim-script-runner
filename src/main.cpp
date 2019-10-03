51892b9bef8eb7f6e31296fce94d50d7a8e40106
AJ Steenhoek <ajsteenhoek@gmail.com>
2019-10-02 23:54:48 -0400

    change

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main ( int argc, char** argv )
     while(true)
         pub.publish(forward);
 
-    //ROS_INFO("Shutting down teleop topic");
+    ROS_INFO("Shutting down teleop topic");
     //pub.shutdown();
     spin();
 }

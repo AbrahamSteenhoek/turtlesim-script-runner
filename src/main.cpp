2d9da09a3f7cdbffa6ceb5111dc87207c35c611f
AJ Steenhoek <ajsteenhoek@gmail.com>
2019-10-16 01:30:00 -0400

    turning function does indeed work (ran it once lol)

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include "geometry_msgs/Twist.h"
 
 const char* gazebo_topic = "/cmd_vel_mux/input/teleop";
+const double PI = std::atan(1.0)*4;
 
 using namespace ros;
 
+//======================================================================================================================
 void OdomCallback( const nav_msgs::Odometry::ConstPtr& msg )
 {
     double x = msg->pose.pose.position.x;
@@ -13,15 +15,15 @@ void OdomCallback( const nav_msgs::Odometry::ConstPtr& msg )
     ROS_INFO( "x: %f, y: %f", x, y );
 }
 
+//======================================================================================================================
 void MoveForward( const Publisher &pub, const double speed, const double distance, const bool isForward=true )
 {
-
+    // for some reason the clock doesn't start for a few milliseconds, so give it some time
     while (Time::now().toSec() <= 0)
     ;
 
     const double t0 = Time::now().toSec();
-    ROS_INFO( "t0 = %f", t0 );
-    double cur_distance = 0;
+    double cur_distance = 0.0;
 
     geometry_msgs::Twist go_the_distance;
 
@@ -38,6 +40,44 @@ void MoveForward( const Publisher &pub, const double speed, const double distanc
         const double t1 = Time::now().toSec();
         cur_distance = speed * ( t1 - t0 );
     }
+
+    go_the_distance.linear.x = 0.0;
+    pub.publish( go_the_distance );
+}
+
+//======================================================================================================================
+// note: parameters require angle units in radians
+void Turn( const Publisher &pub, const double angular_speed_rad, const double relative_angle_final_deg, const bool clockwise )
+{
+    // for some reason the clock doesn't start for a few milliseconds, so give it some time
+    while (Time::now().toSec() <= 0)
+    ;
+    //const double angular_speed_rad = angular_speed_deg * 2 * PI / 360;
+    const double relative_angle_final_rad = relative_angle_final_deg * 2 * PI / 360;
+
+    const double t0 = Time::now().toSec();
+    double cur_angle = 0.0;
+
+    geometry_msgs::Twist turny_turn;
+
+    turny_turn.linear.x = 0.0;
+    turny_turn.linear.y = 0.0;
+    turny_turn.linear.z = 0.0;
+    turny_turn.angular.x = 0.0;
+    turny_turn.angular.y = 0.0;
+
+    turny_turn.angular.z = ( clockwise )? -fabs( angular_speed_rad ) : fabs( angular_speed_rad );
+
+    while ( cur_angle < relative_angle_final_rad )
+    {
+        pub.publish( turny_turn );
+        const double t1 = Time::now().toSec();
+        cur_angle = angular_speed_rad * ( t1 - t0 );
+    }
+
+    turny_turn.angular.z = 0.0;
+    pub.publish( turny_turn );
+    ROS_INFO("done turning!");
 }
 
 int main ( int argc, char** argv )
@@ -55,6 +95,9 @@ int main ( int argc, char** argv )
         ROS_FATAL("Publisher failed. Exiting now...");
     }
 
-    MoveForward( pub, 2, atof( argv[1] ) );
+    MoveForward( pub, 1, 2 );
+    Turn( pub, 1, 90, true );
+    MoveForward( pub, 1, 2, true );
+
     ROS_INFO("end");
 }
